8c0f4719559ea6b4bcb74bf23a9063bb4d4f3ee9
kheladzedev <edward.developer@icloud.com>
2023-04-21 19:32:54 +0300

    Добавлены изменения в RulesViewcontroller

--- SMCroco/View/RulesViewController.swift
+++ SMCroco/View/RulesViewController.swift
@@ -9,60 +9,87 @@ import UIKit
 
 class RulesViewController: UIViewController {
     
-    private let scrollView = UIScrollView()
-    private let backButton = UIButton(type: .system)
-    private let label = UILabel()
+    let scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
+    let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    let headerLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+    
+    lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 0)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 0
+        return button
+    }()
+    
+    let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.textAlignment = .left
+        label.font = UIFont.italicSystemFont(ofSize: 23)
+        label.textColor = .black
+        let rules = Rules()
+        label.text = rules.rules
+        label.sizeToFit()
+        return label
+    }()
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupScrollView()
-        setupBackButton()
-        setupLabel()
+        view.backgroundColor = .green
+        setupViews()
+        setConstraints()
     }
     
-    private func setupScrollView() {
-        scrollView.frame = view.bounds
-        scrollView.backgroundColor = .green
-        scrollView.contentInsetAdjustmentBehavior = .never
-        scrollView.showsVerticalScrollIndicator = true
-        view.addSubview(scrollView)
-    }
-    
-    private func setupBackButton() {
-        backButton.setImage(UIImage(systemName: "chevron.left"), for: .normal)
-        backButton.tintColor = .black
-        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+    private func setupViews() {
+        view.addSubview(headerLabel)
         view.addSubview(backButton)
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            backButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            backButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            backButton.widthAnchor.constraint(equalToConstant: 44),
-            backButton.heightAnchor.constraint(equalToConstant: 44)
-        ])
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(subtitleLabel)
     }
     
-    private func setupLabel() {
-        label.text = "В игру играют командами из двух или более человек. Задача каждого игрока команды - объяснить слово, которое он видит на экране, следуя условиям, которые дополнительно указаны под загаданным словом. Чем больше слов отгадала команда, тем больше она заработает баллов. Выигрывает команда, набравшая больше всего баллов. На отгадывание слова дается одна минута. При нарушении правил объяснения слова, ход передается следующей команде."
-        label.numberOfLines = 0
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 34)
-        label.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 0)
-        label.sizeToFit()
-        scrollView.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
+    private func setConstraints() {
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 40),
-            label.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16),
-            label.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -16),
-            label.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16),
-            label.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -32)
+            headerLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            headerLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            backButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            backButton.widthAnchor.constraint(equalToConstant: 250),
+            
+            scrollView.topAnchor.constraint(equalTo: headerLabel.bottomAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: backButton.topAnchor),
+            scrollView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            subtitleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            subtitleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            subtitleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            subtitleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            subtitleLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
         ])
-    }
-    
-    @objc private func backButtonTapped() {
-        navigationController?.popViewController(animated: true)
     }
 }
-
